d17b0fb88d40804015a25d29efeb42233a112e08
buyan <levistark36@gmail.com>
2024-11-21 11:12:25 +0800

    Nothing Has been added Yet

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick() {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
+}
+</script>
 
 <template>
   <v-responsive class="border rounded" max-height="300">
